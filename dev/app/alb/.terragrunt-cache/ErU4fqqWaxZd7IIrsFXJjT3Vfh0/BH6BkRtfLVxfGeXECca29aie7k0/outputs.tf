c3efdf5ceb52d6e021077b6efbc91c5a2f85ce78
Bohdan <happy4@ukr.net>
2024-04-01 12:19:13 +0300

    removed_terragrunt_cache

--- dev/app/alb/.terragrunt-cache/ErU4fqqWaxZd7IIrsFXJjT3Vfh0/BH6BkRtfLVxfGeXECca29aie7k0/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "load_balancer_dns_name" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.ASG_load_balancer.dns_name
-}
--- dev/app/ec2/.terragrunt-cache/5Gz3WtDQURAxaEucW-5hcCGb9uI/AcLm9laO1yWLNJLKS9yOTOtgWfM/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ASG_launch_config" {
-  description = "lauch configuration name"
-  value       = aws_launch_configuration.ASG_launch_config.name
-}
